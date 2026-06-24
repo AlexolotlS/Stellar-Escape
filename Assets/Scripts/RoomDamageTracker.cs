dcb29d5ebfc0bf117095fef2ea32312c89d85f9e
Kernighan <107226662+King5ive@users.noreply.github.com>
2026-06-23 23:49:31 -0400

    Storage & Gravity Gen room

--- Assets/Scripts/RoomDamageTracker.cs
+++ Assets/Scripts/RoomDamageTracker.cs
@@ -7,15 +7,26 @@ public class RoomDamageTracker : MonoBehaviour
     public float damagedParts;
     [SerializeField] GameObject[] doors;
     [SerializeField] TextMeshProUGUI roomText;
+    [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] bool activateGravity;
 
     public void RoomFixed() {
         roomText.color = Color.green;
         OpenDoors();
+        if (activateGravity) {
+            ActivateGravity();
+        }
     }
 
     public void OpenDoors() {
         foreach (GameObject door in doors) {
             door.SetActive(false);
+        }
+    }
+
+    void ActivateGravity() {
+        if (playerMovement != null) {
+            playerMovement.jumpHeight = 2;
         }
     }
 }
