ec48920f30eb9312fed83963f5278c1923cd0aff
Kernighan <107226662+King5ive@users.noreply.github.com>
2026-06-26 11:03:09 -0400

    UI for level 1

--- Assets/Scripts/PartManager.cs
+++ Assets/Scripts/PartManager.cs
@@ -1,8 +1,11 @@
+using TMPro;
 using UnityEngine;
 
 public class PartManager : MonoBehaviour
 {
     [SerializeField] GameObject portal;
+    [SerializeField] TextMeshProUGUI partCollectedText;
+    [SerializeField] TextMeshProUGUI portalText;
     [SerializeField] int partsNeeded = 5;
     int partsCollected = 0;
 
@@ -11,9 +14,11 @@ public class PartManager : MonoBehaviour
         if (partsCollected >= partsNeeded) {
             ActivatePortal();
         }
+        partCollectedText.text = "Parts Collected: " + partsCollected;
     }
 
     void ActivatePortal() {
         portal.SetActive(true);
+        portalText.gameObject.SetActive(true);
     }
 }
